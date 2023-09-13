4094d203fd308502dac3f14940aaae2fdc73ab01
Aaron <iaaron.xyz@outlook.com>
2023-09-13 15:08:23 -0600

    wirte logif for capitalize function and tests

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,5 +1,5 @@
 function capitalize(string) {
-  return "";
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 module.exports = capitalize;
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,5 +1,21 @@
 const capitalize = require('../functions/capitalize');
 
+// Capitalize a valid string
 test('capitalize mark to equal Mark', () => {
   expect(capitalize('mark')).toBe('Mark');
+})
+
+// empty string returns empty string
+test('Deal with an empty string | "" -> ""', () => {
+  expect(capitalize('')).toBe('');
+})
+
+// In a phrase capitalize just the first word.
+test('Capitalize just the first word | "i code" -> "I code"', () => {
+  expect(capitalize('i code')).toBe('I code');
+})
+
+// Numbers are not captalized
+test('Numbers remain equal', () => {
+  expect(capitalize('12/november/2023')).toBe('12/november/2023');
 })
